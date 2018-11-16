285e81da1c75387c89f02092c9dad846e1e42559
dvatsal <vatsaldin@gmail.com>
2018-11-16 17:19:00 +0530

    Update DeepestPit.java

--- DeepestPit.java
+++ DeepestPit.java
@@ -1,13 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice;
+
+import java.util.TreeSet;
+
+/**
+ *
+ * @author vatsal
+ */
 public class DeepestPit{
 
      public static void main(String []args){
         System.out.println("Hello World");
-        HelloWorld.deepestPit({0,1,3,-2,0,1,0,-3,2,3});
+        int [] A = {0,1,3,-2,0,1,0,-3,2,3};
+        DeepestPit.deepestPit(A);
+        
      }
      
      static void deepestPit(int []A){
          
-         TreeSet<int> depthList = new TreeSet<int>();
+         TreeSet<Integer> depthList;
+         depthList = new TreeSet<Integer>();
          
          for (int P = 0; P < A.length;P++)
          {
@@ -18,10 +34,15 @@ public class DeepestPit{
                     
                  int Q = R-1;
                  
+                 if(Q == 0)
+                     break;
+                 
                  if(A[P]>A[Q] && A[Q]<A[R]){
-                     System.out.println("tuplet P,Q,R:-"+P+" "+Q+" "+R);
-                     System.out.println("Values of P,Q,R:-"+A[P]+" "+A[Q]+" "+A[R]);
-                     System.out.println("Depth->A[P]-A[Q]"+A[P]-A[Q]+"and depth A[R]-A[Q]"+A[R]-A[Q]+" ");
+                     System.out.print("Indices P,Q,R->"+P+" "+Q+" "+R);
+                     System.out.println(" Values->"+A[P]+" "+A[Q]+" "+A[R]);
+                     int temp1 = A[P]-A[Q];
+                     int temp2 = A[R]-A[Q];
+                   System.out.println("Depth->P-Q "+temp1+" Depth->R-Q "+temp2+" "+" MINIMUM "+Math.min(A[P]-A[Q],A[R]-A[Q]));
                      depthList.add(Math.min(A[P]-A[Q],A[R]-A[Q]));
                  }
                  
