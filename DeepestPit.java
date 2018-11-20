814221b47f05afbf2b9ae9940d04c990c8bcfba1
dvatsal <vatsaldin@gmail.com>
2018-11-20 21:58:46 +0530

    Update DeepestPit.java
    solution that worked perfectly

--- DeepestPit.java
+++ DeepestPit.java
@@ -16,42 +16,39 @@ public class DeepestPit{
      public static void main(String []args){
         System.out.println("Hello World");
         int [] A = {0,1,3,-2,0,1,0,-3,2,3};
-        DeepestPit.deepestPit(A);
+        //DeepestPit.deepestPit(A);
+          DeepestPit.Solution(A);
         
      }
      
-     static void deepestPit(int []A){
-         
-         TreeSet<Integer> depthList;
-         depthList = new TreeSet<Integer>();
-         
-         for (int P = 0; P < A.length;P++)
-         {
-             
-             
-             for(int R = A.length-1; R>P; R--)
-             {
-                    
-                 int Q = R-1;
-                 
-                 if(Q == 0)
-                     break;
-                 
-                 if(A[P]>A[Q] && A[Q]<A[R]){
-                     System.out.print("Indices P,Q,R->"+P+" "+Q+" "+R);
-                     System.out.println(" Values->"+A[P]+" "+A[Q]+" "+A[R]);
-                     int temp1 = A[P]-A[Q];
-                     int temp2 = A[R]-A[Q];
-                   System.out.println("Depth->P-Q "+temp1+" Depth->R-Q "+temp2+" "+" MINIMUM "+Math.min(A[P]-A[Q],A[R]-A[Q]));
-                     depthList.add(Math.min(A[P]-A[Q],A[R]-A[Q]));
-                 }
-                 
-                 
-             }
-             
-         }
-         
-         System.out.println("highest depth:-"+depthList.last().toString());
-         
-     }
+   public static int Solution(int []A)
+   {
+    int depth = 0;
+
+    int P = 0, Q = -1, R = -1;
+
+    for (int i = 1; i < A.length; i++)
+    {
+        if (Q < 0 && A[i] >= A[i-1]) 
+            Q = i-1;
+
+        if ((Q >= 0 && R < 0) && 
+            (A[i] <= A[i-1] || i + 1 == A.length))
+        {
+            if (A[i] <= A[i-1])
+                R = i - 1;
+            else
+                R = i;
+            System.out.println(P+"  "+Q+"  "+R);
+            depth = Math.max(depth, Math.min(A[P]-A[Q], A[R]-A[Q]));
+            P = i - 1; 
+            Q = R = -1;
+        }
+    }
+    if (depth == 0) depth = -1;
+    System.out.println("Depth: "+depth);
+        
+    return depth;
+   }
+     
 }
